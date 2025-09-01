060f38dba5104fc82899ffb0cb2fd2662a405b94
dyna <kaleidkirito627@gmail.com>
2025-09-01 17:03:39 +0530

    Update fare graph.py

--- fare graph.py
+++ fare graph.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as pl
-df=pd.read_csv("C:\\Users\\janvi\\Documents\\reservation table.csv")
+df=pd.read_csv("C:\\Users\\Documents\\reservation table.csv")
 print (df);
 pl.plot(df["fare"])
+
 pl.show();
